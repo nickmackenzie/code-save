9320f2f9112ab0321d9d743e3186337700fa419f
nickmackenzie <nick-m@live.ca>
2020-10-21 22:58:34 -0400

    added highlightJS

--- models/snippets.js
+++ models/snippets.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const tagSchema = new Schema({
+    tags: []
+})
 const snippetSchema = new Schema({
+    snippet: String,
     language: String,
     name: String,
-    tags: [],
-    snippet: String,
+    categories: [tagSchema],
     favorite: Boolean
 })
 module.exports = mongoose.model("snippets", snippetSchema)
